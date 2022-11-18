4c25788efa37842475a3d4dba99e64bcb5801552
= <=>
2022-11-17 18:31:07 -0800

    counter with react

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useReducer, useState } from 'react'
+import './App.css'
+
+const getRanmonNumberFromApi = async (): Promise<number> => {
+	const res = await fetch('https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new');
+	const numberString = res.text();
+	/* throw new Error('Auxilio'); */
+	return parseInt(await numberString);
+}
+
+export const App = () => {
+	const [number, setNumber] = useState<number>();
+	const [isLoading, setIsLoading] = useState<boolean>(true);
+	const [error, setError] = useState<string>();
+	const [key, forceRefectch] = useReducer((x) => x + 1, 0);
+
+	useEffect(() => {
+		setIsLoading(true);
+		getRanmonNumberFromApi()
+			.then(num => setNumber(num))
+			.catch(error => setError(error.message));
+	}, [key]);
+
+	useEffect(() => {
+		if (number) setIsLoading(false);
+	}, [number])
+
+	useEffect(() => {
+		if (error) setIsLoading(false);
+	}, [error])
+
+	return (
+		<div className="App">
+			{isLoading
+				? (<h2> Loading... </h2>)
+				: (<h2>Numero aleatorio: {number}</h2>)
+			}
+
+			{
+				!isLoading && error && (<h3>{error}</h3>)
+			}
+
+			<button onClick={() => forceRefectch()} disabled={isLoading}>
+				{isLoading ? '...' : 'New Number'}
+			</button>
+
+		</div>
+	)
+};
